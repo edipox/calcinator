3669b8be4c3724e3145df2c02418dc417633f42a
Luke Imhoff <limhoff@csd.org>
2017-01-25 11:39:15 -0600

    Failing regression test for Subjectless miscapitalization

--- test/support/calcinator/resources/test_author.ex
+++ test/support/calcinator/resources/test_author.ex
@@ -5,6 +5,8 @@ defmodule Calcinator.Resources.TestAuthor do
 
   use Ecto.Schema
 
+  import Ecto.Changeset, only: [cast: 3]
+
   schema "authors" do
     field :name, :string
     field :password, :string, virtual: true
@@ -12,4 +14,8 @@ defmodule Calcinator.Resources.TestAuthor do
 
     has_many :posts, Calcinator.Resources.TestPost, foreign_key: :author_id
   end
+
+  # Functions
+
+  def changeset(model, params), do: cast(model, params, ~w(name password password_confirmation)a)
 end
