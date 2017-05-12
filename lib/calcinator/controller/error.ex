100aa7a2182128f89958e00ac1332a24c5a28911
Luke Imhoff <limhoff@csd.org>
2017-05-11 21:04:54 -0500

    Errors and Error documents
    Make the Alembic.Document and Alembic.Error that
    Calcinator.Controller.Error uses internally available in
    Calcinator.Alembic.Document and Calcinator.Alembic.Error, respectively,
    so they can be reused in overrides and `retort`.

--- lib/calcinator/controller/error.ex
+++ lib/calcinator/controller/error.ex
@@ -5,8 +5,7 @@ if Code.ensure_loaded?(Phoenix.Controller) do
     have same format for errors.
     """
 
-    alias Alembic.{Document, Error, Source}
-    alias Calcinator.ChangesetView
+    alias Calcinator.{Alembic.Document, ChangesetView}
     alias Plug.Conn
 
     require Logger
@@ -18,18 +17,7 @@ if Code.ensure_loaded?(Phoenix.Controller) do
     Retort returned a 500 JSONAPI error inside a 422 JSONRPC error.
     """
     @spec bad_gateway(Conn.t) :: Conn.t
-    def bad_gateway(conn) do
-      render_json conn,
-                  %Document{
-                    errors: [
-                      %Error{
-                        status: "502",
-                        title: "Bad Gateway"
-                      }
-                    ]
-                  },
-                  :bad_gateway
-    end
+    def bad_gateway(conn), do: render_json(conn, Document.bad_gateway(), :bad_gateway)
 
     @doc """
     The resource was deleted
@@ -45,56 +33,19 @@ if Code.ensure_loaded?(Phoenix.Controller) do
     The current resource or action is forbidden to the authenticated user
     """
     @spec forbidden(Conn.t) :: Conn.t
-    def forbidden(conn) do
-      render_json conn,
-                  %Document{
-                    errors: [
-                      %Error{
-                        detail: "You do not have permission for this resource.",
-                        status: "403",
-                        title: "Forbidden"
-                      }
-                    ]
-                  },
-                  :forbidden
-    end
+    def forbidden(conn), do: render_json(conn, Document.forbidden(), :forbidden)
 
     @doc """
     Puts 504 Gateway Timeout JSONAPI error in `conn`.
     """
     @spec gateway_timeout(Conn.t) :: Conn.t
-    def gateway_timeout(conn) do
-      render_json conn,
-                  %Document{
-                    errors: [
-                      %Error{
-                        status: "504",
-                        title: "Gateway Timeout"
-                      }
-                    ]
-                  },
-                  :gateway_timeout
-    end
+    def gateway_timeout(conn), do: render_json(conn, Document.gateway_timeout(), :gateway_timeout)
 
     @doc """
     Puts 404 Resource Not Found JSONAPI error in `conn` with `parameter` as the source parameter.
     """
     @spec not_found(Conn.t, String.t) :: Conn.t
-    def not_found(conn, parameter) do
-      render_json conn,
-                  %Document{
-                    errors: [
-                      %Error{
-                        source: %Source{
-                          parameter: parameter
-                        },
-                        status: "404",
-                        title: "Resource Not Found"
-                      }
-                    ]
-                  },
-                  :not_found
-    end
+    def not_found(conn, parameter), do: render_json(conn, Document.not_found(parameter), :not_found)
 
     @doc """
     Puts 500 Internal Server Error JSONAPI error in `conn` with title `"Ownership Error"`.
@@ -102,22 +53,14 @@ if Code.ensure_loaded?(Phoenix.Controller) do
     @spec ownership_error(Conn.t) :: Conn.t
     def ownership_error(conn) do
       render_json conn,
-                   %Document{
-                     errors: [
-                       %Error{
-                         detail: "Owner of backing store connection could not be found",
-                         status: "500",
-                         title: "Ownership Error"
-                       }
-                     ]
-                   },
-                   # DBConnection.OwnershipError raised when the connection was checked out from the pool too long and
-                   # the lease was # revoked.  This could be a 504 Gateway Timeout, but that pool is inside Elixir and
-                   # not part of the Database # itself, so keeping as 500 Internal Server Error.  504 Gateway Timeout is
-                   # also not accurate, because the # "gateway" is responding, it's just saying you can't do it.
-                   #
-                   # See 5XX section of http://racksburg.com/choosing-an-http-status-code/
-                   :internal_server_error
+                  Document.ownership_error(),
+                  # DBConnection.OwnershipError raised when the connection was checked out from the pool too long and
+                  # the lease was # revoked.  This could be a 504 Gateway Timeout, but that pool is inside Elixir and
+                  # not part of the Database # itself, so keeping as 500 Internal Server Error.  504 Gateway Timeout is
+                  # also not accurate, because the # "gateway" is responding, it's just saying you can't do it.
+                  #
+                  # See 5XX section of http://racksburg.com/choosing-an-http-status-code/
+                  :internal_server_error
     end
 
     @doc """
@@ -130,7 +73,7 @@ if Code.ensure_loaded?(Phoenix.Controller) do
     def put_calcinator_error(conn, {:error, :sandbox_token_missing}), do: sandbox_token_missing(conn)
     def put_calcinator_error(conn, {:error, :timeout}), do: gateway_timeout(conn)
     def put_calcinator_error(conn, {:error, :unauthorized}), do: forbidden(conn)
-    def put_calcinator_error(conn, {:error, document = %Document{}}) do
+    def put_calcinator_error(conn, {:error, document = %Alembic.Document{}}) do
       render_json(conn, document, :unprocessable_entity)
     end
     def put_calcinator_error(conn, {:error, changeset = %Ecto.Changeset{}}) do
@@ -161,23 +104,7 @@ if Code.ensure_loaded?(Phoenix.Controller) do
     """
     @spec render_error_reason(Conn.t, reason :: term) :: Conn.t
     def render_error_reason(conn, reason) do
-      id = UUID.uuid4()
-
-      Logger.error fn ->
-        "id=#{id} reason=#{inspect(reason)}"
-      end
-
-      document = %Document{
-        errors: [
-          %Error{
-            id: id,
-            status: "500",
-            title: "Internal Server Error"
-          }
-        ]
-      }
-
-      render_json(conn, document, :internal_server_error)
+      render_json(conn, Document.error_reason(reason), :internal_server_error)
     end
 
     @doc """
@@ -209,20 +136,7 @@ if Code.ensure_loaded?(Phoenix.Controller) do
     """
     @spec sandbox_access_disallowed(Conn.t) :: Conn.t
     def sandbox_access_disallowed(conn) do
-      render_json conn,
-                  %Document{
-                    errors: [
-                      %Error{
-                        detail: "Information in /meta/beam was not enough to grant access to the sandbox",
-                        source: %Source{
-                          pointer: "/meta/beam"
-                        },
-                        status: "422",
-                        title: "Sandbox Access Disallowed"
-                      }
-                    ]
-                  },
-                  :unprocessable_entity
+      render_json conn, Document.sandbox_access_disallowed(), :unprocessable_entity
     end
 
     @doc """
@@ -230,20 +144,7 @@ if Code.ensure_loaded?(Phoenix.Controller) do
     """
     @spec sandbox_token_missing(Conn.t) :: Conn.t
     def sandbox_token_missing(conn) do
-      render_json conn,
-                  %Document{
-                    errors: [
-                      Error.missing(
-                        %Error{
-                          source: %Source{
-                            pointer: "/meta"
-                          }
-                        },
-                        "beam"
-                      )
-                    ]
-                  },
-                  :unprocessable_entity
+      render_json conn, Document.sandbox_token_missing(), :unprocessable_entity
     end
   end
 end
