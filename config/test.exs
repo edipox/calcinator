35db35d572e57b0b6afbd4ce804a5a7c9a831de5
Luke Imhoff <limhoff@csd.org>
2017-06-30 08:19:39 -0500

    Align no paretheses call arguments

--- config/test.exs
+++ config/test.exs
@@ -1,16 +1,16 @@
 use Mix.Config
 
 config :calcinator, Calcinator.Resources.Ecto.Repo.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  database: "calcinator_test",
-  pool: Ecto.Adapters.SQL.Sandbox
+       adapter: Ecto.Adapters.Postgres,
+       database: "calcinator_test",
+       pool: Ecto.Adapters.SQL.Sandbox
 
 config :calcinator,
-  ecto_repos: [Calcinator.Resources.Ecto.Repo.Repo]
+       ecto_repos: [Calcinator.Resources.Ecto.Repo.Repo]
 
 # Print only warnings and errors during test
 config :logger,
        level: :warn
 
 config :phoenix, :format_encoders,
-  "json-api": Poison
+       "json-api": Poison
