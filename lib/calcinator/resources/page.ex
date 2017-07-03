8e70a277f9beb1fdefbda606996201aa26ba4266
Luke Imhoff <limhoff@csd.org>
2017-07-02 19:03:43 -0500

    Align when to call

--- lib/calcinator/resources/page.ex
+++ lib/calcinator/resources/page.ex
@@ -100,8 +100,8 @@ defmodule Calcinator.Resources.Page do
   defp deprecated_to_params(nil), do: %{}
 
   defp deprecated_to_params(%__MODULE__{number: number, size: size})
-      when is_integer(number) and number > 0 and
-           is_integer(size) and size > 0 do
+       when is_integer(number) and number > 0 and
+            is_integer(size) and size > 0 do
     %{
       "page" => %{
         "number" => number,
