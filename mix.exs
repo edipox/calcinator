2ab44822418e05590b5bb30db2d9be599d463217
Luke Imhoff <limhoff@csd.org>
2017-10-12 15:26:56 -0500

    Update junit_formatter to 2.0.0

--- mix.exs
+++ mix.exs
@@ -100,7 +100,7 @@ defmodule Calcinator.Mixfile do
       {:faker, "~> 0.9.0", only: :test},
       {:ja_serializer, ">= 0.11.2 and < 0.13.0"},
       # JUnit formatter, so that CircleCI can consume test output for CircleCI UI
-      {:junit_formatter, "~> 1.0", only: :test},
+      {:junit_formatter, "~> 2.0", only: :test},
       # Phoenix.Controller is used in Calcinator.Controller.Error
       {:phoenix, "~> 1.0", optional: true},
       # Testing PryIn instrumenter
